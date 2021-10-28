7e64c0e687fc5803368f657e52604bf1de1b8241
Andrew <Andrew@DESKTOP-AAM9PD1>
2021-10-28 14:53:49 +0900

    First Commit!!!

---
+++ Springboot/src/main/java/com/example/boot/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootApplication {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "HELLO, WORLD!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
